6d691596a2d67b3748a89fc82ea300792daa7b88
bs-arno <arno@bsnetworks.net>
2018-06-29 16:32:04 +0800

    Flask框架学习

--- app.py
+++ app.py
@@ -6,7 +6,17 @@ app = Flask(__name__)
 @app.route('/')
 def hello_user():
     user = {'name': 'Arno'}
-    return render_template("hello-user.html", title='Home', user=user)
+    posts = [
+        {
+            'author': {'name': 'Ar'},
+            'body': 'This is test!'
+        },
+        {
+            'author': {'name': 'no'},
+            'body': 'No!'
+        }
+    ]
+    return render_template("hello-user.html", title='Home', user=user, posts=posts)
 
 
 if __name__ == '__main__':
