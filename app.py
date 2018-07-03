dd819e2b3ebb2d0d12aa7c5defef385fa6de9019
bs-arno <arno@bsnetworks.net>
2018-07-03 14:29:42 +0800

    数据库连接,用户登录

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, flash, redirect
+from forms import LoginForm
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config.from_object('config')
+db = SQLAlchemy(app)
+import models
 
 
 @app.route('/')
@@ -17,6 +23,15 @@ def hello_user():
         }
     ]
     return render_template("hello-user.html", title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
+        return redirect('/')
+    return render_template('login.html', title='Sign In', form=form, providers=app.config['OPENID_PROVIDERS'])
 
 
 if __name__ == '__main__':
